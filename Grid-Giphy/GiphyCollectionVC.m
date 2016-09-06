41c07220f9ac22374b9071df13ed32d5852d2a21
Michael Jasinski <michael.jasinski@detroitlabs.com>
2016-09-06 09:48:15 -0400

    Clean logs and code format

--- Grid-Giphy/GiphyCollectionVC.m
+++ Grid-Giphy/GiphyCollectionVC.m
@@ -29,47 +29,44 @@
 }
 
 - (void)americanPsychoMethod {
-    NSString *urlString = @"http://api.giphy.com/v1/gifs/search?q=american+psycho&api_key=dc6zaTOxFJmzC&limit=10";
+    NSString *urlString = @"http://api.giphy.com/v1/gifs/search?q=american+psycho&api_key=dc6zaTOxFJmzC&limit=100";
     
     NSURL *giphyUrl = [NSURL URLWithString:urlString];
     
     NSURLRequest *request = [NSURLRequest requestWithURL:giphyUrl];
-//    NSLog(@"Request %@", request);
     
     NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
-//    NSLog(@"Config: %@", config);
     
     NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
-//    NSLog(@"SESSION: %@", session);
     
- NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {         NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) { NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
         
         if (httpResponse.statusCode == 200) {
             NSError *JSONError;
-            NSMutableDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&JSONError];
-//            NSLog(@"Dictionay: %@", dict);
             
-//            NSArray *responseArray = [dict valueForKeyPath:@"data.images.downsized.url"];
+            NSMutableDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&JSONError];
+            
             giphyArray = [dict valueForKeyPath:@"data.images.downsized.url"];
             
             dispatch_async(dispatch_get_main_queue(), ^{
                 
-                for (NSString *giph in giphyArray) {
-//                    NSURL *URLString = [NSURL URLWithString:giph];
-//                    NSData *data = [NSData dataWithContentsOfURL:URLString];
-//                    UIImage *psychoImage = [[UIImage alloc] initWithData:data];
+                for (NSString *giphString in giphyArray) {
+                    // NSURL *URLString = [NSURL URLWithString:giph];
+                    // NSData *data = [NSData dataWithContentsOfURL:URLString];
                     
-                    FLAnimatedImage *image = [FLAnimatedImage animatedImageWithGIFData:[NSData dataWithContentsOfURL:[NSURL URLWithString:giph]]];
+                    //Short Hand version
+                    FLAnimatedImage *giph = [FLAnimatedImage animatedImageWithGIFData:[NSData dataWithContentsOfURL:[NSURL URLWithString:giphString]]];
                     
-                    NSLog(@"IMAGE = %@", image);
-                    [finalArray addObject:image];
-//                    [finalArray addObject:psychoImage];
+                    [finalArray addObject:giph];
+                    
+                    [self.collectionView reloadData];
                     
                 }
-            
-                NSLog(@"Final Array Call 2: %@", finalArray);
+                
                 [self.collectionView reloadData];
+            
             });
+            
             [self.collectionView reloadData];
             
         } else {
@@ -81,28 +78,18 @@
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    NSLog(@"Final Array Table: %@", finalArray);
     return finalArray.count;
     
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     NSString *identifier = @"Cell";
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
     
-//    FLAnimatedImageView *imageView = [[FLAnimatedImageView alloc] init];
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:identifier forIndexPath:indexPath];
+
     FLAnimatedImageView *imageView = (FLAnimatedImageView *)[cell viewWithTag:100];
     
     imageView.animatedImage = [finalArray objectAtIndex:indexPath.row];
-//    imageView.frame = CGRectMake(0.0, 0.0, 100.0, 100.0);
-//    [self.view addSubview:imageView];
-//
-//    UIImageView *giphyImageView = (UIImageView *)[cell viewWithTag:100];
-//    giphyImageView.image = [finalArray objectAtIndex:indexPath.row];
-//    
-    
-   
-    
     
     return cell;
 }
